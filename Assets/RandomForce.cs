71c73d888f77d936e08b2e8cc3a4b7d1f56bbd80
vasyoid <vasyoid@gmail.com>
2021-05-07 07:58:04 +0300

    Add sample project

---
+++ Assets/RandomForce.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomForce : MonoBehaviour
+{
+    public float delay;
+    public float magnitude;
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+            Vector3 dir = Random.insideUnitSphere;
+            dir.y = Math.Abs(dir.y);
+            rb.AddForce(dir * magnitude);
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
